0c36966630fd5fe42bf6ae16a6e41aa1e0f42cb6
Josh Gwinn <jdgwinn911@gmail.com>
2018-12-06 11:20:55 -0500

    fixed the axis of grid

--- board.rb
+++ board.rb
@@ -1,27 +1,30 @@
 require_relative "ship.rb"
-class Place
-    def initialize(x, y)
-      @cord = [x, y]
+# class Place
+#     def initialize(x, y)
+#       @cord = [x, y]
 
-    end
-    attr_reader :cord
-end
+#     end
+#     attr_reader :cord
+# end
 
-class Grid < Place
+class Grid 
     def initialize(size = 12)
-        @grid = Array.new(size, Array.new(size, ""))
+        @grid = Array.new(size) {Array.new(size, "")}
     end
 
     def place(x, y)
-        ship = Place.new(x, y)
-        @grid[ship.cord[0]][ship.cord[1]] = ship
+        ship = "ship"
+        @grid[x][y] = ship
     end
+    attr_reader :x
+    attr_reader :y
     attr_reader :grid
     attr_reader :size
 end
 
 o = Grid.new(24)
-o.place(4,2)
+o.place(4,5)
 o.grid.each do |v|
     p v
 end
+p o.grid[6][10]
