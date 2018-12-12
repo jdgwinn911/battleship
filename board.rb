d988216223dea1725482d60f68d71d530d014199
Josh Gwinn <jdgwinn911@gmail.com>
2018-12-12 13:29:25 -0500

    made a semi aligned grid and labled it

--- board.rb
+++ board.rb
@@ -2,7 +2,6 @@ require_relative "ship.rb"
 require_relative "cell.rb"
 
 class Grid < Cell
-
     def initialize(size = 12)
         @grid = Array.new(size) {Array.new(size) {Cell.new}}
         @end_point = size - 1
@@ -14,15 +13,30 @@ class Grid < Cell
     attr_reader :end_point
 end
 
+
 o = Grid.new()
-o.grid[1][2].place_on_cell(Ship.new(2,"Ubot"))
-o.grid.each do |v|
-    v.each_with_index do |k,i|
-        if i == o.end_point
-            print "|#{k.to_s}| \n"
+counter = 1 
+letters = 
+('A'..'Z').to_a
+print "  "
+
+o.grid.each_with_index do |v, i|
+    print "  #{letters[i]}"
+end
+puts "\n"
+o.grid[1][2].place_on_cell(Ship.new(2,"Bot"))
+
+
+o.grid.each_with_index do |v, i|
+    v.each_with_index do |k, i|
+        if i == 0 
+            print " #{counter} #{k.to_s}"
+            counter += 1   
+        elsif i == o.end_point
+            print "#{k.to_s} \n"
         else
-            print "|#{k.to_s}|"
+            print "#{k.to_s}"
         end
     end
 end
-print o.grid[2][2].content.to_s
+# print o.grid[2][2].content.to_s
--- cell.rb
+++ cell.rb
@@ -2,24 +2,33 @@ require_relative "ship.rb"
 class Cell 
     def initialize()
         @status = nil
-        @content = "water" 
+        @content = "(-)" 
         @size = size 
     end
 
     def if_hit()
-        if @content =="water"
+        if @content =="(-)"
             @status =  "missed"
         elsif @content == ship
            @status = ship.take_a_hit()
         end
     end
 
-    def place_on_cell(ship, direction)
+    def place_on_cell(ship)
         @content = ship
+        # if direction == "up"
+        #     size[1] + 1
+        # elsif direction == "down"
+        #     size[0] - 1
+        # elsif direction == "left"
+        #     size[-1] - 1
+        # elsif direction == "right"
+        # end
+
     end
 
     def to_s()
-        @content == "water" ? @content : @content.name
+        @content == "(-)" ? @content : @content.name
     end
     attr_reader :size
     attr_reader :coordinates
