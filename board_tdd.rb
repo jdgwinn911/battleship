ac0b7b5badc572382763e0cc26cb4a0611cde156
Josh Gwinn <jdgwinn911@gmail.com>
2018-12-17 10:47:25 -0500

    passed some more tests for ship placement

--- board_tdd.rb
+++ board_tdd.rb
@@ -20,5 +20,25 @@ class Board < Minitest::Test
         assert_equal("ship", o.grid[row][col].content())
     end
     
+    def test_that_ship_can_be_placed2
+        o = Grid.new
+        row = 4
+        col = 3 
+        o.place_ship(Ship.new(4, "battleship"),row, col, "horizontal")
+        o.place_ship(Ship.new(5, "carrier"),row, col, "horizontal")
+        assert_equal("ship", o.grid[row][col].content())
+        assert_equal("ship", o.grid[row][col].content())
+    end
+
+    
+    def test_that_ship_can_be_placed3
+        o = Grid.new
+        row = 4
+        col = 3 
+        o.place_ship(Ship.new(3, "cruiser"),row, col, "horizontal")
+        o.place_ship(Ship.new(5, "carrier"),row, col, "horizontal")
+        assert_equal("ship", o.grid[row][col].content())
+        assert_equal("ship", o.grid[row][col].content())
+    end
 
 end
