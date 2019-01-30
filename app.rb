109c92d1b05e888bf5cf6aa7aae4390348c3280d
Josh Gwinn <jdgwinn911@gmail.com>
2019-01-30 15:41:10 -0500

    can successfully place ships on my grid, now need to account for enemy grid then rules to keep people from breaking

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ use Rack::Session::Pool, :expire_after => 60 * 1
 
 
 get '/' do
-  session[:board] = Grid.new(12, "player")
   erb :bs1
 end
 
@@ -26,8 +25,8 @@ end
 
 post '/page2' do
   
-  session[:board] = Grid.new(params[:gridsize, "player")
-  session[:enemyboard] = Grid.new(params[:gridsize], "ai")
+  session[:board] = Grid.new(params[:gridsize].to_i, "player")
+  session[:enemyboard] = Grid.new(params[:gridsize].to_i, "ai")
   redirect '/page3'
 end
 
@@ -38,17 +37,17 @@ get '/page3' do
   row = session[:row].to_i || 0
   col = session[:col].to_i || 0
   pos = session[:pos].to_s || ""
-  board.mastor_func(session[:place_ship], row, col, pos) if pos != ""
+  board.mastor_funk(session[:place_ship], row, col, pos) if pos != ""
   erb :bs3, locals: {board: board, enemyboard: enemyboard, row: row, col: col, pos: pos, ship_num: ship_num}
 end
 
 post '/page3' do
-  session[:increase] = params[:ship_num]
+  session[:increase] = params[:ship_num].to_i
   session[:row] = params[:row]
   session[:col] = params[:col]
   session[:pos] = params[:pos]
   ships = [carrier = Ship.new(5, "(C)"), battleship = Ship.new(4, "(B)"), cruiser = Ship.new(3, "(c)"), submarine = Ship.new(2, "(S)")]
-  session[:place_ship] = ships[increase]
+  session[:place_ship] = ships[session[:increase]]
   session[:increase] += 1
   redirect '/page3'
 end
